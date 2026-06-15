d44c6567108d108f3fde19aa2fbe1ba86ffd3f31
Tri Pham <phamminhtris@outlook.com>
2026-06-15 12:35:53 -0500

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.swift
@@ -0,0 +1,26 @@
+class Solution {
+    func isAnagram(_ s: String, _ t: String) -> Bool {
+        guard s.count == t.count else { 
+            return false
+        }
+        var charDict = [Character: Int]()
+        for char in s {
+            charDict[char, default: 0] += 1
+        }
+
+        for char in t {
+            if let charCount = charDict[char] {
+                let newCount = charCount - 1
+                if newCount == 0 {
+                    charDict[char] = nil
+                } else {
+                    charDict[char] = newCount
+                }
+            } else {
+                return false
+            }
+        }
+
+        return charDict.isEmpty
+    }
+}
